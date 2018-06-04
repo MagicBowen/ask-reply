cda97ee7f8fc3e75e454953d09e1672f1b3e772f
magicbowen <e.bowen.wang@icloud.com>
2018-06-04 13:50:19 +0800

    setup server

---
+++ index.js
@@ -0,0 +1,34 @@
+const AixBot = require('aixbot');
+
+const aixbot = new AixBot('123');
+
+aixbot.use(async (ctx, next) => {
+    console.log(`process request for '${ctx.request.query}' ...`);
+    var start = new Date().getTime();
+    await next();
+    var execTime = new Date().getTime() - start;
+    console.log(`... response in duration ${execTime}ms`);
+});
+
+aixbot.onEvent('enterSkill', (ctx) => {
+    ctx.query('你好，请开始录音').record();
+});
+
+aixbot.onEvent('recordFinish', (ctx) => {
+    let asr = ctx.request.body.request.event_property.asr_text;
+    let msgId = ctx.request.body.request.event_property.msg_file_id;
+    console.log(`query in record finish : ${ctx.request.query}`);
+    console.log(`asr text in record finish : ${asr}`);
+    console.log(`msg id in record finish : ${msgId}`);
+    ctx.speak(asr).playMsgs([msgId]).wait();
+});
+
+aixbot.onEvent('recordFail', (ctx) => {
+    ctx.query('录音失败，请重录').record();
+});
+
+aixbot.onEvent('quitSkill', (ctx) => {
+    ctx.reply('再见').closeSession();
+});
+
+aixbot.run(8080);
