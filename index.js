dbdcf24a249a4fb21e60eaf6ffb08e5906c2c471
WeiGangqiang <284894567@qq.com>
2018-06-12 11:45:51 +0800

    fix bug

--- index.js
+++ index.js
@@ -23,8 +23,13 @@ aixbot.use(async (ctx, next) => {
             if (res.data[0].type === 'play-record') {
                 const fileId = res.data[0]['file-id'];
                 const needRecord = ((res.data.length > 1) && (res.data[1].type === 'start-record'));
-                if (needRecord) return ctx.directiveTts(res.reply).directiveRecord(fileId).record();
-                if (fileId) return ctx.query(res.reply).playMsgs([fileId]);
+                if (needRecord) {
+                    if (fileId && fileId !== '') {
+                        return ctx.directiveTts(res.reply).directiveRecord(fileId).record();
+                    } 
+                    return ctx.query(res.reply + ':' + res.data[0].content).record();
+                }
+                if (fileId && fileId !== '') return ctx.query(res.reply).playMsgs([fileId]);
                 return ctx.query(res.reply + ':' + res.data[0].content);
             }
         }
